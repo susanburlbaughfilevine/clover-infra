99a4d48d2d80e2c1edcb6ae2e6fd6acf1b63d439
Abby Malson <abbymalson@filevine.com>
2020-12-14 16:00:35 -0700

    update variables and output

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
-output "instance_ips" {
-    value = ["${module.cloverdx.instance_ips}"]
-}
+#output "instance_ips" {
+#    value = ["${module.cloverdx.instance_ips}"]
+#}
 
-output "check" {
-    #    value = element(module.cloverdx.instance_ids, 1)
-    # count = instance_ids
-    value = module.cloverdx.instance_ids
-}
+#output "check" {
+#    #    value = element(module.cloverdx.instance_ids, 1)
+#    # count = instance_ids
+#    value = module.cloverdx.instance_ids
+#}
 #output "check-lookup" {
   # value = lookup([{"instance_id"=module.cloverdx.instance_ids}], "instance_id", "what")
 #  value = "-${element(
