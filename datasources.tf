43ab40da5dae3cbd6d653442396da9f1bcc762c7
Abby Malson <abbymalson@filevine.com>
2020-12-14 14:42:41 -0700

    copying over some logic from main filevine terraform

---
+++ datasources.tf
@@ -0,0 +1,61 @@
+# Security Groups
+data "aws_iam_account_alias" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_vpc" "filevine" {
+  filter {
+    name   = "tag-key"
+    values = ["Name"]
+  }
+  filter {
+    name   = "tag-value"
+    values = ["${data.aws_iam_account_alias.current.account_alias}"]
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.filevine.id
+
+  tags = {
+    Tier = "private"
+  }
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.filevine.id
+
+  tags = {
+    Tier = "public"
+  }
+}
+
+data "aws_kms_alias" "frontend" {
+  name = "alias/fv/server"
+}
+
+data "aws_security_group" "frontend" {
+  name = "${var.envName}-FrontEnd"
+}
+
+data "aws_security_group" "techaccess" {
+  name = "${var.envName}-TechAccess"
+}
+
+data "aws_security_group" "dataaccess" {
+  name = "${var.envName}-DatastoresAccess"
+}
+
+data "aws_security_group" "frontend-loadbalancer" {
+  name = "${var.envName}-FrontEnd-LB"
+}
+
+data "aws_security_group" "build" {
+  name = "${var.envName}-Build"
+}
+
+data "aws_s3_bucket" "accesslogs" {
+  bucket = "${data.aws_iam_account_alias.current.account_alias}-lblogs-${var.region}"
+}
