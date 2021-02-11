38c6bc8b8084eaca124209a1b99642097c4bcd12
Abby Malson <abbymalson@filevine.com>
2021-02-11 12:48:36 -0700

    adding session token

--- templates/download_assets_from_s3.ps1
+++ templates/download_assets_from_s3.ps1
@@ -1,8 +1,9 @@
 # $s3_secret_key        = $s3_secret_key
 # $s3_access_key        = $s3_access_key
 Write-Output "Debug: --------------------------"
-Write-Output "S3 secret Key: $filevine_devops_secret_key"
-Write-Output "S3 Access key: $filevine_devops_access_key"
+Write-Output "S3 secret Key:  $filevine_devops_secret_key"
+Write-Output "S3 Access key:  $filevine_devops_access_key"
+Write-Output "S3 Session Key: $filevine_devops_session_token"
 Write-Output "End Debug: ----------------------"
 # =======================================================================
 $clover_assets        = "C:\clover_assets"
@@ -20,12 +21,20 @@ if (Test-Path $clover_assets) {
 
 Push-Location $clover_assets 
 
-Write-Output "Secret Key: ${filevine_devops_secret_key}"
-Write-Output "Acccess Key: ${filevine_devops_access_key}"
+#Write-Output "Secret Key: ${filevine_devops_secret_key}"
+#Write-Output "Acccess Key: ${filevine_devops_access_key}"
+Set-Variable AWS_ACCESS_KEY_ID="${filevine_devops_access_key}"
+Set-Variable AWS_SECRET_ACCESS_KEY="${filevine_devops_secret_key}"
+Set-Variable AWS_SESSION_TOKEN="${filevine_devops_session_token}"
+
+
 
 # FIXME: Move this stuff to Artifactory
 # Let's grab our assets from S3
-Set-AWSCredentials -AccessKey "${filevine_devops_access_key}" -SecretKey "${filevine_devops_secret_key}"
+# Set-AWSCredentials -AccessKey "${filevine_devops_access_key}" -SecretKey "${filevine_devops_secret_key}"
+
+# Let's verify the caller identity ...
+aws sts get-caller-identity
 
 $objects = get-S3object -bucketname $srcBucketName
 Read-S3Object -BucketName '$srcBucketName' -KeyPrefix cloverdx-assets/ -Folder '$clover_assets'
