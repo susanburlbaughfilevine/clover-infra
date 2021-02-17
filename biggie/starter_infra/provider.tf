5d531140f5a287c41dad7a9150229e8e81fc7b10
Austin Patterson <austinpatterson@filevine.com>
2021-02-17 13:33:46 -0500

    feature/deploy_rds: Update to use the correct provider file

--- biggie/starter_infra/provider.tf
+++ biggie/starter_infra/provider.tf
@@ -1,5 +1,23 @@
 provider "aws" {
-  region  = "us-west-2"
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
   alias = "clover"
 }
 
+provider "octopusdeploy" {
+  address = var.octopus_provider_server_address
+  apikey  = var.octopus_api_key
+  space   = var.octopus_space
+}
+
+terraform {
+  backend "s3" {
+    bucket               = "fv-global-fv-tf-backend"
+    key                  = "clover-starter-infra"
+    dynamodb_table       = "fv-global-fv-tf-backend-table"
+    region               = "us-west-2"
+    encrypt              = true
+    workspace_key_prefix = "clover-starter-infra-workspace"
+  }
+}
