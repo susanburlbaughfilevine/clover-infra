0c64577fb6e9dbca18d0d71fdae4128efbaff016
Austin Patterson <austinpatterson@filevine.com>
2021-02-18 11:30:33 -0500

    feature/deploy_rds: Remove all duplicate resources

--- biggie/rds/datasources.tf
+++ biggie/rds/datasources.tf
@@ -2,38 +2,6 @@ data "aws_iam_account_alias" "current" {}
 
 data "aws_region" "current" {}
 
-data "aws_vpc" "clover" {
-  filter {
-    name   = "tag-key"
-    values = ["Name"]
-  }
-  filter {
-    name   = "tag-value"
-    values = [data.aws_iam_account_alias.current.account_alias]
-  }
-}
-
-data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.clover.id
-
-  tags = {
-    Tier = "private"
-  }
-}
-
-data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.clover.id
-
-  tags = {
-    Tier = "public"
-  }
-}
-
-
-data "aws_kms_alias" "sqlserver" {
-  name = "alias/fv/default"
-}
-
 data "aws_security_group" "sqlserver" {
   name = "${var.envName}-clover-DatastoresAccess"
 }
@@ -42,8 +10,6 @@ data "aws_sns_topic" "slack_sns_topic" {
   name = "${var.envName}-clover-slack-alerting"
 }
 
-data "aws_region" "current" {}
-
 data "aws_vpc" "clover" {
   filter {
     name   = "tag-key"
@@ -74,12 +40,4 @@ data "aws_subnet_ids" "public" {
 
 data "aws_kms_alias" "sqlserver" {
   name = "alias/fv/default"
-}
-
-data "aws_security_group" "sqlserver" {
-  name = "${var.envName}-DatastoresAccess"
-}
-
-data "aws_sns_topic" "slack_sns_topic" {
-  name = "${var.envName}-slack-alerting"
 }
