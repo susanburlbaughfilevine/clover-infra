b505b159701a4129fa81de1e605c6c08423a3d0f
Austin Patterson <austinpatterson@filevine.com>
2021-02-20 22:15:45 -0500

    feature/deploy_rds: Add clover whitelist

---
+++ clover_whitelist.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "clover_whitelist" {
+  name        = "${var.envName}-clover-whitelist"
+  description = "Whitelist for CloverDX"
+  vpc_id      = data.aws_vpc.clover.id
+
+  tags = {
+    Name       = "${var.envName}-Clover-DX"
+    managed_by = "Octopus via Terraform"
+  }
+
+  ingress {
+    description = "Austin Patterson Home"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    self        = true
+    cidr_blocks = ["76.192.9.181/32"]
+  }
+}
