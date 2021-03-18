bdbe1acc55532e22521ccbdde2269548237609ed
Abby Malson <abbymalson@filevine.com>
2021-03-18 14:34:17 -0600

    update the datasources

--- datasources.tf
+++ datasources.tf
@@ -1,11 +1,12 @@
 # Security Groups
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_account_alias" "current" {}
 
 data "aws_region" "current" {
   name = var.region
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_account_alias" "current" {}
 
 data "aws_vpc" "clover" {
   filter {
