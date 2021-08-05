eb81669c09ea94b388b8057c445a691e133b8a4e
Austin Patterson <austinpatterson@filevine.com>
2021-08-05 12:16:23 -0400

    Add TTL to terraform resource

--- clover_fv_web.tf
+++ clover_fv_web.tf
@@ -98,10 +98,6 @@ resource "aws_route53_record" "clover_internal_record" {
   zone_id  = data.aws_route53_zone.master.id
   name     = "internal-clover-cjis.${var.dns_domain}"
   type     = "CNAME"
-
-  alias {
-    name                   = aws_lb.clover_alb_internal.dns_name
-    zone_id                = aws_lb.clover_alb_internal.zone_id
-    evaluate_target_health = true
-  }
+  ttl      = 300
+  records  = ["${aws_lb.clover_alb_internal.dns_name}"]
 }
