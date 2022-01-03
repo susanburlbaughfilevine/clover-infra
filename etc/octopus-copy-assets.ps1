47a47d41c48a920ac2556de66ea445af1e1f5d0f
Austin Patterson <austinpatterson@filevine.com>
2022-01-03 16:40:17 -0500

    Add files neccessary for windows asset deployment

---
+++ etc/octopus-copy-assets.ps1
@@ -0,0 +1,2 @@
+# Used by Octopus project "Deploy CloverDX Assets - Windows" to deploy assets to CloverDX server
+Expand-Archive -Path cloverdx-windows\clover-assets.zip -DestinationPath $env:SYSTEMDRIVE\ -Force
