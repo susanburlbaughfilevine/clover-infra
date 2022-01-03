c0657dfe3532f37e6d4e28b9a4725ec68b6de0f8
Austin Patterson <austinpatterson@filevine.com>
2022-01-03 15:14:55 -0500

    Remove local provider file changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,30 +1,26 @@
 provider "aws" {
   #  region  = "us-west-2"
   #  profile = "fv-us"
-  profile = "lockpick"
-  region  = "us-west-2"
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
 }
 
 provider "aws" {
-  alias   = "filevine"
-  profile = "filevine"
-  region  = "us-west-2"
+  alias = "filevine"
 }
 
 provider "aws" {
-  profile = "platform"
-  alias   = "platform"
-  region  = "us-west-2"
-  # assume_role {
-  #   role_arn = "arn:aws:iam::358974996326:role/Terraform"
-  # }
+  alias = "platform"
+  assume_role {
+    role_arn = "arn:aws:iam::358974996326:role/Terraform"
+  }
 }
 
 # this sets up future configuration to use the remote backend created in the backend directory
 # Note that the workspaces prefix doesn't allow interpolation, so this is the same as what's set in variables
 terraform {
   backend "s3" {
-    profile              = "filevine"
     bucket               = "fv-global-fv-tf-backend"
     key                  = "filevine-clover-servers"
     dynamodb_table       = "fv-global-fv-tf-backend-table"
