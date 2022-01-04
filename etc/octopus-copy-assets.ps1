ff3755c9633d2f0445af4bd547dd1f60a1ca0c37
Austin Patterson <austinpatterson@filevine.com>
2022-01-04 14:20:26 -0500

    Delete copy script until we hear back from Octopus Support

--- etc/octopus-copy-assets.ps1
+++
@@ -1,3 +0,0 @@
-# Used by Octopus project "Deploy CloverDX Assets - Windows" to deploy assets to CloverDX server
-dir
-Expand-Archive -Path cloverdx-windows\clover-assets.zip -DestinationPath $env:SYSTEMDRIVE\ -Force
