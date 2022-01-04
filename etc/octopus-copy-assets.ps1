15c72185fde03ed2751df6eaf7b81e19fd3817a1
Austin Patterson <austinpatterson@filevine.com>
2022-01-04 13:36:41 -0500

    List directory contents for troubleshooting

--- etc/octopus-copy-assets.ps1
+++ etc/octopus-copy-assets.ps1
@@ -1,2 +1,3 @@
 # Used by Octopus project "Deploy CloverDX Assets - Windows" to deploy assets to CloverDX server
+dir
 Expand-Archive -Path cloverdx-windows\clover-assets.zip -DestinationPath $env:SYSTEMDRIVE\ -Force
