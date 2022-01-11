9bdcc081e712da69b467d4d99a3ec01590d07fec
Austin Patterson <austinpatterson@filevine.com>
2022-01-11 14:04:41 -0500

    Fix configurecloverassets

--- etc/ConfigureCloverAssets.ps1
+++ etc/ConfigureCloverAssets.ps1
@@ -1,24 +1,24 @@
-$config = Import-PowershellDataFile c:\clover-assets\clover-assets-manifest.psd1
+$config = Import-PowershellDataFile $env:SYSTEMDRIVE\clover-assets\clover-assets-manifest.psd1
 
 # Delete old JDK and Tomcat directories. If we need to revert due to an issue, deploy an older release
-Remove-Item C:\jdk -Recurse -Force
-Remove-Item C:\tomcat -Recurse -Force
+if (Test-Path "$($env:SYSTEMDRIVE)\jdk") {Remove-Item $env:SYSTEMDRIVE\jdk -Recurse -Force}
+if (Test-Path "$($env:SYSTEMDRIVE)\tomcat") {Remove-Item $env:SYSTEMDRIVE\tomcat -Recurse -Force}
 
 # Extract/Install JDK and Apache Tomcat
-$tomcatDirectory = New-Item -Type Directory -Path c:\tomcat
-$jdkDirectory = New-Item -Type Directory -Path c:\jdk
+$tomcatDirectory = New-Item -Type Directory -Path $env:SYSTEMDRIVE\tomcat
+$jdkDirectory = New-Item -Type Directory -Path $env:SYSTEMDRIVE\jdk
 $jdkPath = Join-Path -Path $jdkDirectory.FullName -ChildPath ($config["jdk"].PackageName).Replace(".zip","")
 $tomcatPath = Join-Path -Path $tomcatDirectory.FullName -ChildPath $config["tomcat"].PackageName.Replace(".zip","")
-Expand-Archive C:\clover-assets\$($config["tomcat"].PackageName) -Destination $tomcatDirectory.FullName
-Expand-Archive C:\clover-assets\$($config["jdk"].PackageName) -Destination $jdkDirectory.FullName
+Expand-Archive $env:SYSTEMDRIVE\clover-assets\$($config["tomcat"].PackageName) -Destination $tomcatDirectory.FullName
+Expand-Archive $env:SYSTEMDRIVE\clover-assets\$($config["jdk"].PackageName) -Destination $jdkDirectory.FullName
 
 # Configure Tomcat installation
 [Environment]::SetEnvironmentVariable("JAVA_HOME", "$jdkPath\bin", "Machine")
 [Environment]::SetEnvironmentVariable("JRE_HOME", "$jdkPath", "Machine")
 $env:JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine")
 $env:JRE_HOME = [System.Environment]::GetEnvironmentVariable("JRE_HOME","Machine")
-Copy-Item -Path C:\clover-assets\cloverServer.properties -Destination "$tomcatPath\conf\"
-$setEnvScript = (Get-Content -Path C:\clover-assets\setenv.bat).Replace("##tomcatConfDir##","$tomcatPath\conf\cloverServer.properties")
+Copy-Item -Path $env:SYSTEMDRIVE\clover-assets\cloverServer.properties -Destination "$tomcatPath\conf\"
+$setEnvScript = (Get-Content -Path $env:SYSTEMDRIVE\clover-assets\setenv.bat).Replace("##tomcatConfDir##","$tomcatPath\conf\cloverServer.properties")
 New-Item -Type File -Path "$tomcatPath\bin\setenv.bat" -Value $setEnvScript
 
 #Start-Process -FilePath $tomcatPath\bin\startup.bat -Wait
