d62ba407f3e4c5c1267b8357d5d0d4c1e8645c73
Austin Patterson <austinpatterson@filevine.com>
2022-01-11 17:11:52 -0500

    WAR file extraction

--- etc/ConfigureCloverAssets.ps1
+++ etc/ConfigureCloverAssets.ps1
@@ -18,7 +18,17 @@ Expand-Archive $env:SYSTEMDRIVE\clover-assets\$($config["jdk"].PackageName) -Des
 $env:JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine")
 $env:JRE_HOME = [System.Environment]::GetEnvironmentVariable("JRE_HOME","Machine")
 Copy-Item -Path $env:SYSTEMDRIVE\clover-assets\config\cloverServer.properties -Destination "$tomcatPath\conf\"
+Copy-Item -Path $env:SYSTEMDRIVE\clover-assets\config\clover-server.xml -Destination "$tomcatPath\conf\"
 $setEnvScript = (Get-Content -Path $env:SYSTEMDRIVE\clover-assets\config\setenv.bat).Replace("##tomcatConfDir##","$tomcatPath\conf\cloverServer.properties")
 New-Item -Type File -Path "$tomcatPath\bin\setenv.bat" -Value $setEnvScript
+
+# CloverDX Server and Profiler Server Installation
+New-Item -Type Directory -Path $tomcatPath\webapps\clover\
+Set-Location $tomcatPath\webApps\clover\
+& "$($env:JAVA_HOME)\jar.exe" -xvf $env:SYSTEMDRIVE\clover-assets\clover.war
+
+New-Item -Type Directory -Path $tomcatPath\webapps\profiler
+Set-Location $tomcatPath\webApps\profiler\
+& "$($env:JAVA_HOME)\jar.exe" -xvf $env:SYSTEMDRIVE\clover-assets\profiler.war
 
 #Start-Process -FilePath $tomcatPath\bin\startup.bat -Wait
