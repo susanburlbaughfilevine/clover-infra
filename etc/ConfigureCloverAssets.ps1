10010c1bd30b54f493496628ee192f29c46ac184
Austin Patterson <austinpatterson@filevine.com>
2022-01-11 13:38:49 -0500

    Start working on the configure stage

---
+++ etc/ConfigureCloverAssets.ps1
@@ -0,0 +1,24 @@
+$config = Import-PowershellDataFile c:\clover-assets\clover-assets-manifest.psd1
+
+# Delete old JDK and Tomcat directories. If we need to revert due to an issue, deploy an older release
+Remove-Item C:\jdk -Recurse -Force
+Remove-Item C:\tomcat -Recurse -Force
+
+# Extract/Install JDK and Apache Tomcat
+$tomcatDirectory = New-Item -Type Directory -Path c:\tomcat
+$jdkDirectory = New-Item -Type Directory -Path c:\jdk
+$jdkPath = Join-Path -Path $jdkDirectory.FullName -ChildPath ($config["jdk"].PackageName).Replace(".zip","")
+$tomcatPath = Join-Path -Path $tomcatDirectory.FullName -ChildPath $config["tomcat"].PackageName.Replace(".zip","")
+Expand-Archive C:\clover-assets\$($config["tomcat"].PackageName) -Destination $tomcatDirectory.FullName
+Expand-Archive C:\clover-assets\$($config["jdk"].PackageName) -Destination $jdkDirectory.FullName
+
+# Configure Tomcat installation
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "$jdkPath\bin", "Machine")
+[Environment]::SetEnvironmentVariable("JRE_HOME", "$jdkPath", "Machine")
+$env:JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine")
+$env:JRE_HOME = [System.Environment]::GetEnvironmentVariable("JRE_HOME","Machine")
+Copy-Item -Path C:\clover-assets\cloverServer.properties -Destination "$tomcatPath\conf\"
+$setEnvScript = (Get-Content -Path C:\clover-assets\setenv.bat).Replace("##tomcatConfDir##","$tomcatPath\conf\cloverServer.properties")
+New-Item -Type File -Path "$tomcatPath\bin\setenv.bat" -Value $setEnvScript
+
+#Start-Process -FilePath $tomcatPath\bin\startup.bat -Wait
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "clover_server_ip" {
   value = aws_instance.clover.private_ip
 }
+
+output "rds_instance_address" {
+  value = aws_db_instance.sqlserver.address
+}
