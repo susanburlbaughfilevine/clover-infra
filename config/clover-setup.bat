3eb57a746b70752d49f384ac5f3c253c66c22885
Austin Patterson <austinpatterson@filevine.com>
2022-01-12 14:53:33 -0500

    Fix encoding on the clover-setup.bat file

--- config/clover-setup.bat
+++ config/clover-setup.bat
@@ -1,6 +1,6 @@
-CALL SET JvmArgs="-Dclover.config.file=##tomcatConfDir## -Djdk.nio.maxCachedBufferSize=262144 "
+CALL SET JvmArgs="-Dclover.config.file=##tomcatConfDir## -Djdk.nio.maxcacheBufferSize=262144"
 
-CALL SET CATALINA_OPTS="USE_DIRECT_MEMORY=false -XX:ReservedCodeCacheSize=256m -XX:MaxMetaspaceSize=512m -Xms128m -Xmx1024m"
+CALL SET CATALINA_OPTS="USE_DIRECT_MEMORY=false -XX:ReservedCodeCacheSize-256m -XX:MaxMetaspaceSize=512m -Xms128m -Xmx1024m"
 
 REM CALL CD %CATALINA_HOME%\bin
 REM service install "cloverDx"
