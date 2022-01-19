39da95b5df5478ad3c78509d4a647ff764410e82
Austin Patterson <austinpatterson@filevine.com>
2022-01-19 09:27:48 -0500

    Provide an explicit, default scaling configuration

--- terraform/rds-mssql.tf
+++ terraform/rds-mssql.tf
@@ -19,21 +19,28 @@ locals {
 }
 
 resource "aws_rds_cluster" "sqlserver" {
-  cluster_identifier              = lower("${var.envName}-cloverdx")
-  availability_zones              = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  engine                          = "aurora-postgresql"
-  engine_mode                     = "serverless"
-  master_username                 = var.rds_user_name
-  master_password                 = var.rds_user_password
-  backup_retention_period         = 35
-  preferred_backup_window         = "07:00-08:00"
-  vpc_security_group_ids          = [aws_security_group.dataaccess.id]
-  storage_encrypted               = true
-  kms_key_id                      = data.aws_kms_alias.default.target_key_arn
-  deletion_protection             = false
-  db_subnet_group_name            = aws_db_subnet_group.sqlserver.name
-  copy_tags_to_snapshot           = true
-  preferred_maintenance_window    = "sun:09:00-sun:11:00"
+  cluster_identifier           = lower("${var.envName}-cloverdx")
+  availability_zones           = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  engine                       = "aurora-postgresql"
+  engine_mode                  = "serverless"
+  master_username              = var.rds_user_name
+  master_password              = var.rds_user_password
+  backup_retention_period      = 35
+  preferred_backup_window      = "07:00-08:00"
+  vpc_security_group_ids       = [aws_security_group.dataaccess.id]
+  storage_encrypted            = true
+  kms_key_id                   = data.aws_kms_alias.default.target_key_arn
+  deletion_protection          = false
+  db_subnet_group_name         = aws_db_subnet_group.sqlserver.name
+  copy_tags_to_snapshot        = true
+  preferred_maintenance_window = "sun:09:00-sun:11:00"
+
+  scaling_configuration {
+    max_capacity             = 16
+    min_capacity             = 2
+    seconds_until_auto_pause = 300
+    timeout_action           = "RollbackCapacityChange"
+  }
 }
 
 resource "aws_db_subnet_group" "sqlserver" {
