47fd829cee0788569ed6020622866e86a12a5b0a
Austin Patterson <austinpatterson@filevine.com>
2022-05-19 17:28:42 -0400

    Write out password encryption command to troubleshoot

--- etc/Install-CloverDxServer.psm1
+++ etc/Install-CloverDxServer.psm1
@@ -80,6 +80,7 @@ function Install-CloverDxServer
     $c = "cmd.exe /c encrypt.bat -a PBEWITHSHA256AND256BITAES-CBC-BC -c org.bouncycastle.jce.provider.BouncyCastleProvider -l $($packageDir)\$($config["bouncycastle"].PackageName) --batch $($DbInstancePassword)"
     Write-Host "Executing: $c"
     $encryptedPass = Invoke-Expression $c
+    Write-Host "Result $encryptedPass"
 
     $serverProperties = (Get-Content -Path $packageDir\config\cloverServer.properties)
     $serverProperties = $serverProperties.Replace("##cryptoProviderLocation##","$($tomcatPath)\webapps\clover\WEB-INF\lib\")
