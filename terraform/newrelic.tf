135b031288240e164d355e5e3e5fd2667912db7b
Austin Patterson <austinpatterson@filevine.com>
2022-07-11 14:36:21 -0400

    Add new relic tf provider Try users config apply on each deployment

---
+++ terraform/newrelic.tf
@@ -0,0 +1,9 @@
+resource "newrelic_alert_channel" "warnings" {
+  name = "${var.envName}-alert-channel"
+  type = "slack"
+
+  config {
+    url     = var.nr_slack_webhook
+    channel = var.nr_slack_channel
+  }
+}
