46ee241a4f54c150f97a88d4bef830eeb85ae6d3
Austin Patterson <austinpatterson@filevine.com>
2022-11-30 17:08:35 -0500

    Hardcode secret name due to misleading IAM errors

--- terraform/clover_instanceprofile.tf
+++ terraform/clover_instanceprofile.tf
@@ -88,7 +88,7 @@ resource "aws_iam_policy" "secrets_manager_access" {
           "secretsmanager:UpdateSecret"
         ]
         Effect   = "Allow"
-        Resource = "arn:aws:secretsmanager:*:*:secret:*"
+        Resource = "*"
       },
       {
         Sid      = "ListAllSecrets",
